032f35c0d84feaae82a5d4e5aae3067cf64a99b7
numan.shakir <numan.shakir@gmail.com>
2023-09-28 11:55:15 +0500

    feat: Task Done

--- lib/App/Home/View/homepage.dart
+++ lib/App/Home/View/homepage.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     :homeVM.moviesList.isNotEmpty? MoviesList(
                         moviesList: homeVM.moviesList.value,
 
-                      ):const Text("No Movies Found"))),
+                      ):Center(child: const Text("No Movies Found")))),
             h20,
             h20,
           ],
